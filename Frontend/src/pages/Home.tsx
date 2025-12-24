df921188478a1b91628c2c6dfdea0e2f3d09ecda
Yasindu <ramrasu2017@gmail.com>
2025-12-24 22:16:32 +0530

    create frontend

---
+++ Frontend/src/pages/Home.tsx
@@ -0,0 +1,21 @@
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+import Categories from '../components/Categories';
+import HowItWorks from '../components/HowItWorks';
+import Testimonials from '../components/Testimonials';
+import CTA from '../components/CTA';
+import Footer from '../components/Footer';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Navbar />
+      <Hero />
+      <Categories />
+      <HowItWorks />
+      <Testimonials />
+      <CTA />
+      <Footer />
+    </div>
+  );
+}
