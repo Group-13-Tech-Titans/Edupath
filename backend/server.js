dc70a3b85d1d22ec6d001cf496545c27f9e8da3e
yasindugunasekara <yasinduramrasu2020@gmail.com>
2026-04-19 15:31:38 +0530

    add DNS server configuration in server setup

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 
 const authRoutes = require("./auth/routes/authRoutes");  
