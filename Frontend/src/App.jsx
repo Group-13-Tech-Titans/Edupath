1f4098474690b3bbe151c61339213f52cea713b0
Nirmani Gamage <nirmanibuddhini2002@gmail.com>
2026-04-22 00:55:19 +0530

    feat: initialize frontend project structure and configure application routing for all user roles

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,42 +1,40 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
+
+// Layouts
 import PublicLayout from "./components/layouts/PublicLayout.jsx";
 import PlainLayout from "./components/layouts/PlainLayout.jsx";
 import StudentLayout from "./components/layouts/StudentLayout.jsx";
 import EducatorLayout from "./components/layouts/EducatorLayout.jsx";
 import AdminLayout from "./components/layouts/AdminLayout.jsx";
 import ReviewerLayout from "./components/layouts/ReviewerLayout.jsx";
+import MentorLayout from "./components/layouts/MentorLayout.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
-import AdminCourseReview from "./pages/admin/AdminCourseReview.jsx";
 
+// Pages
 import Home from "./pages/Home.jsx";
 import Login from "./pages/Login.jsx";
-import SignupRole from "./pages/SignupRole.jsx";
-import SignupStudent from "./pages/SignupStudent.jsx";
-import SignupEducator from "./pages/SignupEducator.jsx";
-import PublicCourses from "./pages/PublicCourses.jsx";
-
-import StudentDashboard from "./pages/student/StudentDashboard.jsx";
-
-import Home from "./pages/Home.jsx";
-import Login from "./pages/Login.jsx";
-import Signup from "./pages/Signup.jsx";
 import SignupRole from "./pages/SignupRole.jsx";
 import SignupStudent from "./pages/SignupStudent.jsx";
 import SignupEducator from "./pages/SignupEducator.jsx";
 import ForgotPassword from "./pages/ForgotPassword.jsx";
 import ResetPassword from "./pages/ResetPassword.jsx";
+import PublicCourses from "./pages/PublicCourses.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import ComingSoon from "./pages/ComingSoon.jsx";
 
+// Student Pages
+import StudentDashboard from "./pages/student/StudentDashboard.jsx";
 import StudentCourses from "./pages/student/StudentCourses.jsx";
 import StudentCourseDetail from "./pages/student/StudentCourseDetail.jsx";
 import StudentMentor from "./pages/student/StudentMentor.jsx";
 import StudentProfile from "./pages/student/StudentProfile.jsx";
 import StudentPathway from "./pages/student/StudentPathway.jsx";
 import StudentStepDetail from "./pages/student/StudentStepDetail";
-import StudentDashboard from "./pages/student/StudentDashboard.jsx"
 import PathFinder from "./pages/student/PathFinder.jsx";
 
+// Educator Pages
 import EducatorDashboard from "./pages/educator/EducatorDashboard.jsx";
 import EducatorCourses from "./pages/educator/EducatorCourses.jsx";
 import EducatorPublish from "./pages/educator/EducatorPublish.jsx";
@@ -46,23 +44,27 @@ import EducatorPayouts from "./pages/educator/EducatorPayouts.jsx";
 import EducatorProfile from "./pages/educator/EducatorProfile.jsx";
 import EducatorVerificationPendingPage from "./pages/educator/EducatorVerificationPendingPage.jsx";
 
+// Admin Pages
 import AdminDashboard from "./pages/admin/AdminDashboard.jsx";
 import AdminReviewers from "./pages/admin/AdminReviewers.jsx";
 import AdminVerifyEducators from "./pages/admin/AdminVerifyEducators.jsx";
 import AdminReviewDashboard from "./pages/admin/AdminReviewDashboard.jsx";
+import AdminCourseReview from "./pages/admin/AdminCourseReview.jsx";
 import AdminProfile from "./pages/admin/AdminProfile.jsx";
 import AdminPathwayBuilder from "./pages/admin/AdminPathwayBuilder.jsx";
 import AdminPathwayList from "./pages/admin/AdminPathwayList.jsx";
 import AdminPathwayEdit from "./pages/admin/AdminPathwayEdit.jsx";
 
+// Reviewer Pages
 import ReviewerDashboard from "./pages/reviewer/ReviewerDashboard.jsx";
 import ReviewerQueue from "./pages/reviewer/ReviewerQueue.jsx";
 import ReviewerCourseReview from "./pages/reviewer/ReviewerCourseReview.jsx";
 import ReviewerHistory from "./pages/reviewer/ReviewerHistory.jsx";
-import AdminProfile from "./pages/admin/AdminProfile.jsx";
+import ReviewerPathwayList from "./pages/reviewer/ReviewerPathwayList.jsx";
+import ReviewerPathwayBuilder from "./pages/reviewer/ReviewerPathwayBuilder.jsx";
+import ReviewerPathwayEdit from "./pages/reviewer/ReviewerPathwayEdit.jsx";
 
-import MentorLayout from "./components/layouts/MentorLayout.jsx";
-
+// Mentor Pages
 import MentorDashboard from "./pages/mentor/MentorDashboard.jsx";
 import MentorProfile from "./pages/mentor/MentorProfile.jsx";
 import MentorSessions from "./pages/mentor/MentorSessions.jsx";
@@ -74,87 +76,54 @@ import MentorMessages from "./pages/mentor/MentorMessages.jsx";
 import MentorStudentDetails from "./pages/mentor/MentorStudentDetails.jsx";
 import MentorAnalytics from "./pages/mentor/MentorAnalytics.jsx";
 
-import ComingSoon from "./pages/ComingSoon.jsx";
-import ReviewerPathwayList from "./pages/reviewer/ReviewerPathwayList.jsx";
-import ReviewerPathwayBuilder from "./pages/reviewer/ReviewerPathwayBuilder.jsx";
-import ReviewerPathwayEdit from "./pages/reviewer/ReviewerPathwayEdit.jsx";
-
-
-import NotFound from "./pages/NotFound.jsx";
-
 const App = () => {
   return (
     <AnimatePresence mode="wait">
       <Routes>
-  <Route path="/" element={<PublicLayout />}>
-    <Route index element={<Home />} />
-    <Route path="login" element={<Login />} />
-    <Route path="signup" element={<SignupRole />} />
-    <Route path="signup/student" element={<SignupStudent />} />
-    <Route path="signup/educator" element={<SignupEducator />} />
-    <Route path="courses" element={<PublicCourses />} />
-        <Route path="/" element={<PlainLayout />}>
-      <Route index element={<Home />} />
-      <Route path="login" element={<Login />} />
-      <Route path="forgot-password" element={<ForgotPassword />} />
-      <Route path="reset-password/:token" element={<ResetPassword />} />
-      <Route path="signup" element={<Signup />} />
-      <Route path="signup/role" element={<SignupRole />} />
-      <Route path="signup/student" element={<SignupStudent />} />
-      <Route path="signup/educator" element={<SignupEducator />} />
-    </Route>
+        {/* Public Routes */}
+        <Route path="/" element={<PublicLayout />}>
+          <Route index element={<Home />} />
+          <Route path="courses" element={<PublicCourses />} />
+        </Route>
 
-    <Route element={<ProtectedRoute allowedRoles={["student"]} />}>
-      <Route path="/student" element={<StudentLayout />}>
-        <Route index element={<StudentDashboard />} />
-        <Route path="courses" element={<StudentCourses />} />
-        <Route path="courses/:id" element={<StudentCourseDetail />} />
-        <Route path="mentor" element={<StudentMentor />} />
-        <Route path="profile" element={<StudentProfile />} />
-            <Route path="/student/path-finder" element={<PathFinder />} />
-            <Route path="/student/journey" element={<StudentPathway />} />
-            <Route path="/student/journey/step/:stepOrder" element={<StudentStepDetail />} />
-          </Route >
-        </Route >
+        <Route element={<PlainLayout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="forgot-password" element={<ForgotPassword />} />
+          <Route path="reset-password/:token" element={<ResetPassword />} />
+          <Route path="signup" element={<SignupRole />} />
+          <Route path="signup/student" element={<SignupStudent />} />
+          <Route path="signup/educator" element={<SignupEducator />} />
+        </Route>
 
-  <Route element={<ProtectedRoute allowedRoles={["educator"]} />}>
-          <Route path="verification-pending" element={<EducatorVerificationPendingPage />} />
-<Route path="/educator" element={<EducatorLayout />}>
-  <Route index element={<EducatorDashboard />} />
-  <Route path="courses" element={<EducatorCourses />} />
-  <Route path="publish" element={<EducatorPublish />} />
-  <Route path="edit/:id" element={<EducatorEditCourse />} />
-  <Route path="add-content/:id" element={<EducatorAddContent />} />
-  <Route path="payouts" element={<EducatorPayouts />} />
-  <Route path="profile" element={<EducatorProfile />} />
-</Route>
-        </Route >
-
-{/* <Route>
-          <Route path="/mentor" element={<MentorLayout />}>
-            <Route index element={<MentorDashboard />} />
-            <Route path="sessions" element={<MentorSessions />} />
-            <Route path="resources" element={<MentorShareResource />} />
-            <Route path="students" element={<MentorStudents />} />
-            <Route path="profile" element={<MentorProfile />} />
+        {/* Student Protected Routes */}
+        <Route element={<ProtectedRoute allowedRoles={["student"]} />}>
+          <Route path="/student" element={<StudentLayout />}>
+            <Route index element={<StudentDashboard />} />
+            <Route path="courses" element={<StudentCourses />} />
+            <Route path="courses/:id" element={<StudentCourseDetail />} />
+            <Route path="mentor" element={<StudentMentor />} />
+            <Route path="profile" element={<StudentProfile />} />
+            <Route path="path-finder" element={<PathFinder />} />
+            <Route path="journey" element={<StudentPathway />} />
+            <Route path="journey/step/:stepOrder" element={<StudentStepDetail />} />
           </Route>
-        </Route> */}
+        </Route>
 
-{/* Mentor test routes (unprotected) - quick access for development/testing */ }
-        <Route path="/MentorDashboard" element={<MentorDashboard />} />
-        <Route path="/MentorSessions" element={<MentorSessions />} />
-        <Route path="/MentorStudents" element={<MentorStudents />} />
-        <Route path="/MentorProfile" element={<MentorProfile />} />
-        <Route path="/MentorSettings" element={<MentorSettings />} />
-        <Route path="/MentorShareProfile" element={<MentorShareProfile />} />
-        <Route path="/MentorResources" element={<MentorResources />} />
-        <Route path="/MentorMessages" element={<MentorMessages />} />
-        <Route path="/MentorStudentDetails/:id" element={<MentorStudentDetails />} />
-        <Route path="/MentorAnalytics" element={<MentorAnalytics />} />
-        
+        {/* Educator Protected Routes */}
+        <Route element={<ProtectedRoute allowedRoles={["educator"]} />}>
+          <Route path="/educator/verification-pending" element={<EducatorVerificationPendingPage />} />
+          <Route path="/educator" element={<EducatorLayout />}>
+            <Route index element={<EducatorDashboard />} />
+            <Route path="courses" element={<EducatorCourses />} />
+            <Route path="publish" element={<EducatorPublish />} />
+            <Route path="edit/:id" element={<EducatorEditCourse />} />
+            <Route path="add-content/:id" element={<EducatorAddContent />} />
+            <Route path="payouts" element={<EducatorPayouts />} />
+            <Route path="profile" element={<EducatorProfile />} />
+          </Route>
+        </Route>
 
-
-
+        {/* Admin Protected Routes */}
         <Route element={<ProtectedRoute allowedRoles={["admin"]} />}>
           <Route path="/admin" element={<AdminLayout />}>
             <Route index element={<AdminDashboard />} />
@@ -165,32 +134,47 @@ const App = () => {
             <Route path="queue/:id" element={<ReviewerCourseReview />} />
             <Route path="approvals" element={<AdminCourseReview />} />
             <Route path="profile" element={<AdminProfile />} />
-            <Route path="profile" element={<AdminProfile />} />
-            <Route path="/admin/pathway-builder" element={<AdminPathwayBuilder />} />
-            <Route path="/admin/pathways" element={<AdminPathwayList />} />
-            <Route path="/admin/pathway-edit/:id" element={<AdminPathwayEdit />} />
-          </Route >
-        </Route >
+            <Route path="pathway-builder" element={<AdminPathwayBuilder />} />
+            <Route path="pathways" element={<AdminPathwayList />} />
+            <Route path="pathway-edit/:id" element={<AdminPathwayEdit />} />
+          </Route>
+        </Route>
 
-  <Route element={<ProtectedRoute allowedRoles={["reviewer"]} />}>
-    <Route path="/reviewer" element={<ReviewerLayout />}>
-      <Route index element={<ReviewerDashboard />} />
-      <Route path="queue" element={<ReviewerQueue />} />
-      <Route path="queue/:id" element={<ReviewerCourseReview />} />
-      <Route path="history" element={<ReviewerHistory />} />
-            <Route path="/reviewer/pathways" element={<ReviewerPathwayList />} />
-            <Route path="/reviewer/pathway-builder" element={<ReviewerPathwayBuilder />} />
-            <Route path="/reviewer/pathway-edit/:id" element={<ReviewerPathwayEdit />} />
-          </Route >
-        </Route >
+        {/* Reviewer Protected Routes */}
+        <Route element={<ProtectedRoute allowedRoles={["reviewer"]} />}>
+          <Route path="/reviewer" element={<ReviewerLayout />}>
+            <Route index element={<ReviewerDashboard />} />
+            <Route path="queue" element={<ReviewerQueue />} />
+            <Route path="queue/:id" element={<ReviewerCourseReview />} />
+            <Route path="history" element={<ReviewerHistory />} />
+            <Route path="pathways" element={<ReviewerPathwayList />} />
+            <Route path="pathway-builder" element={<ReviewerPathwayBuilder />} />
+            <Route path="pathway-edit/:id" element={<ReviewerPathwayEdit />} />
+          </Route>
+        </Route>
 
-        <Route path="*" element={<NotFound />} />
+        {/* Mentor Routes (Keep these for now, maybe add Protection later) */}
+        <Route path="/mentor" element={<MentorLayout />}>
+          <Route index element={<MentorDashboard />} />
+          <Route path="sessions" element={<MentorSessions />} />
+          <Route path="students" element={<MentorStudents />} />
+          <Route path="profile" element={<MentorProfile />} />
+          <Route path="settings" element={<MentorSettings />} />
+          <Route path="share-profile" element={<MentorShareProfile />} />
+          <Route path="resources" element={<MentorResources />} />
+          <Route path="messages" element={<MentorMessages />} />
+          <Route path="student-details/:id" element={<MentorStudentDetails />} />
+          <Route path="analytics" element={<MentorAnalytics />} />
+        </Route>
+
+        {/* Misc Routes */}
         <Route path="/coming-soon" element={<ComingSoon />} />
-
-      </Routes >
-    </AnimatePresence >
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </AnimatePresence>
   );
 };
 
 export default App;
+
 
