c034215b3b8c5728250b29803d7c9d81fd99a11e
Daniru De Silva <165991056+DaniruDesilva@users.noreply.github.com>
2026-08-17 17:45:47 +0530

    Delete Frontend directory

--- Frontend/src/api/specializationApi.js
+++
@@ -1,6 +0,0 @@
-import { apiRequest } from "./client.js";
-
-export async function getSpecializations() {
-  const data = await apiRequest("/api/specializations");
-  return data.specializations;
-}
--- Frontend/src/pages/educator/EducatorCourseDetail.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import CourseInnerPage from "../course/CourseInnerPage.jsx";
-
-const EducatorCourseDetail = () => <CourseInnerPage mode="educator" />;
-
-export default EducatorCourseDetail;
-
